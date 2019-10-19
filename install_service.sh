172488f0d391bce89163cbef6b1268c8c4a4ef83
lupodellasleppa <lupodellasleppa@gmail.com>
2019-10-19 20:18:50 +0200

    Added script to install/reload service unit

---
+++ install_service.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$(id -u)" != "0" ]; then
+  echo "Run this as root."
+  exit 1
+fi
+
+SERVICE_TO_INSTALL=${1?"Please enter a service file to install."}
+
+if [ ! -e /lib/systemd/system/$SERVICE_TO_INSTALL ];
+  cp $SERVICE_TO_INSTALL /lib/systemd/system/$SERVICE_TO_INSTALL
+fi
+
+chmod 644 /lib/systemd/system/$SERVICE_TO_INSTALL
+systemctl daemon-reload
