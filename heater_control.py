49becf8e9e2520bb23bd6507e2d4146b8780bb3e
lupodellasleppa <lupodellasleppa@gmail.com>
2019-10-20 01:45:18 +0200

    Module relay is more generic for relays
    Everythng related to heater control is now in heater_control.py

---
+++ heater_control.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from relay import Relay
+
+def turn_heater_on(max_time=3600):
+    '''
+    Main function to turn the heater on.
+
+    :param max_time: Is the maximum time the heater will be on,
+                    after which it will turn itself off automatically.
+    '''
+
+    heater_switch = Relay(36)
+    heater_switch.on()
+    catch_sleep(max_time)
+    heater_switch.clean()
+
+
+if __name__ == '__main__':
+    import sys
+
+    max_time = 3600
+
+    if len(sys.argv) > 1:
+        try:
+            max_time = int(sys.argv[1])
+        except ValueError as e:
+            content = e.args[0]
+            invalid = content[content.find(':')+2:]
+            raise ValueError(
+                "Argument cannot be interpreted as an integer: {}".format(
+                    invalid
+                )
+            )
+
+    turn_heater_on(max_time)
