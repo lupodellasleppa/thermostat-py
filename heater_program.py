45c1bcf51b28bb68546fd6c1af5b153acbcf44eb
lupodellasleppa <lupodellasleppa@gmail.com>
2019-10-20 21:49:47 +0200

    Added module to control thermostat programs
    It can add and edit programs.
    If not program is found, an example is provided
    (in it, everything is set to false for now)

--- heater_program.py
+++ heater_program.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-import datetime
 import json
 import os
 
 
-class Auto(object):
+class Program(object):
 
-    def __init__(self):
+    def __init__(self, program_number):
 
         self.days_of_week = {
             0: "monday",
@@ -21,17 +20,23 @@ class Auto(object):
 
         repo_path = '/home/pi/raspb-scripts/'
 
-        self.program_settings_path = os.path.join(repo_path, 'program.json')
+        self.program_path = os.path.join(repo_path, 'program.json')
         self.program_example_path = os.path.join(
             repo_path, 'example_program.json'
         )
 
-        if not os.path.isfile(self.program_settings_path):
+        if not os.path.isfile(self.program_path):
             with open(self.program_example_path) as r:
                 program_example = json.load(r)
             program_example = {0: program_example}
 
-        self.program_settings = self.read_program()
+        self.program = self.load_program(program_number)
+
+    def load_program(self, program_number):
+
+        program = self.read_program()
+
+        return program['program_number']
 
     def edit_program(self, program_number, day, hour, value):
         '''
@@ -69,7 +74,7 @@ class Auto(object):
 
         # check program number exists
         try:
-            program_settings = self.read_program()[program_number]
+            program = self.read_program()[program_number]
         except KeyError:
             raise KeyError("Could not find specified program number.")
 
@@ -81,41 +86,41 @@ class Auto(object):
                 # check hour
                 assert h in set(range(24)), invalid_hour_message
                 # insert value
-                program_settings[day][hour] = value
+                program[day][hour] = value
 
-        self.write_program(program_settings)
+        self.write_program(program)
 
     def add_program(mode='new', program_to_copy=None):
 
         assert mode in {'new', 'copy'}, "Only 'new' and 'copy' modes allowed"
 
-        program_settings = read_program()
-        latest_program = max(program_settings)
+        program = read_program()
+        latest_program = max(program)
         with open(self.program_example_path) as f:
             example_program = json.load(f)
 
         if mode == 'new':
-            program_settings[latest_program + 1] = example_program
-            write_program(program_settings)
+            program[latest_program + 1] = example_program
+            write_program(program)
         else:
             assert program_to_copy is not None, (
                 "An integer must be assigned to program_to_copy"
                 " when mode='copy' is set."
             )
-            program_settings[latest_program + 1] = program_settings[
+            program[latest_program + 1] = program[
                 program_to_copy
             ]
-            write_program(program_settings)
+            write_program(program)
 
 
     def read_program(self):
 
-        with open(self.program_settings_path) as f:
-            program_settings = json.load(f)
+        with open(self.program_path) as f:
+            program = json.load(f)
 
-        return program_settings
+        return program
 
-    def write_program(self, program_settings):
+    def write_program(self, program):
 
-        with open(self.program_settings_path, 'w') as f:
-            f.write(json.dumps(program_settings))
+        with open(self.program_path, 'w') as f:
+            f.write(json.dumps(program))
