1924a0e7ed1b2f763a85f5c9832d47d496f5eccc
lupodellasleppa <lupodellasleppa@gmail.com>
2019-10-20 21:51:27 +0200

    Big refactoring after inclusion of auto mode
    Too many changes to list, but the code is pretty self-explanatory

--- heater_control.py
+++ heater_control.py
@@ -2,52 +2,30 @@
 
 import argparse
 import datetime
+from heater_program import Program
+import logging
 from relay import Relay
 
 
-def get_max_time():
-    '''
-    Uses argparse to get desidered time in total_seconds()
-    '''
-
-    parser = argparse.ArgumentParser(description='Start heater.')
-
-    parser.add_argument(
-        '-H', '--hours',
-        help='Number of hours.',
-        type=int,
-        default=0
-    )
-    parser.add_argument(
-        '-M', '--minutes',
-        help='Number of minutes.',
-        type=int,
-        default=0
-    )
-    parser.add_argument(
-        '-S', '--seconds',
-        help='Number of seconds.',
-        type=int,
-        default=0
-    )
-
-    args = parser.parse_args()
-
-    max_time = datetime.timedelta(
-        hours=args.hours,
-        minutes=args.minutes,
-        seconds=args.seconds
-    ).total_seconds()
-
-    if not max_time:
-        parser.error(
-            "Please insert at least one between hours, minutes or seconds."
-        )
-
-    return max_time
+logger_name = 'thermostat'
+logging.basicConfig(
+    format='{levelname:<8} {asctime} - {message}',
+    style='{'
+)
+logger = logging.getLogger(logger_name)
+logger.setLevel(logging.INFO)
 
 
-def turn_heater_on(max_time=3600):
+def get_now():
+
+    current_time = datetime.datetime.now()
+    current_day = program.days_of_week[current_time.weekday()]
+    current_hour = current_time.hour
+
+    return current_day, current_hour
+
+
+def turn_heater_on(mode, program_number=0):
     '''
     Main function to turn the heater on.
 
@@ -55,14 +33,72 @@ def turn_heater_on(max_time=3600):
                     after which it will turn itself off automatically.
     '''
 
+    assert mode in {'auto', 'manual'}
+
     heater_switch = Relay(36)
-    heater_switch.on()
-    heater_switch.catch_sleep(max_time)
-    heater_switch.off()
-    heater_switch.clean()
+
+    if mode == 'auto':
+        # load program
+        program = Program(program_number).program
+        while True:
+            # check each loop for when we are in history
+            current_day, current_hour = get_now()
+            if ( # if current day and hour is True in program
+                program[current_day][current_hour] and
+                # and heater not on
+                not heater_switch.stats
+            ):
+                # start it
+                heater_switch.on()
+            elif ( # if otherwise day and hour is False
+                not program[current_day][current_hour] and
+                # but heater is on
+                heater_switch.stats
+            ):
+                # stop it
+                heater_switch.off()
+            # finally, wait for 5 minutes
+            heater_switch.catch_sleep(300)
+    else:
+        while True:
+            if not heater_switch.stats:
+                heater_switch.on()
+            heater_switch.catch_sleep(300)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description=(
+            'Start heater according to mode and program number'
+            ' if mode is auto.'
+        )
+    )
+
+    parser.add_argument(
+        '-m', '--mode',
+        help='Heater mode [auto|manual].',
+        type=str,
+        required=True
+    )
+
+    parser.add_argument(
+        '-p', '--program',
+        help='Program number to start.'
+        type=int,
+        default=None
+    )
+
+    args = parser.parse_args()
+
+    if args.mode == 'auto' and args.program is None:
+        raise argparse.error(
+            "A program number must be specified if run in auto mode."
+        )
+
+    turn_heater_on(args.mode, args.program)
 
 
 if __name__ == '__main__':
 
-    max_time = get_max_time()
-    turn_heater_on(max_time)
+    main()
