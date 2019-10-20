cb6230b8efe3f6e40ee8531ff4a57ab3636c1f45
lupodellasleppa <lupodellasleppa@gmail.com>
2019-10-20 12:48:00 +0200

    Overwrite service if already present.

--- install_service.sh
+++ install_service.sh
@@ -7,9 +7,7 @@ fi
 
 SERVICE_TO_INSTALL=${1?"Please enter a service file to install."}
 
-if [ ! -e /lib/systemd/system/$SERVICE_TO_INSTALL ]; then
-  cp $SERVICE_TO_INSTALL /lib/systemd/system/$SERVICE_TO_INSTALL
-fi
+cp $SERVICE_TO_INSTALL /lib/systemd/system/$SERVICE_TO_INSTALL
 
 chmod 644 /lib/systemd/system/$SERVICE_TO_INSTALL
 systemctl daemon-reload
