bda97fad14217c722ffd26fe0b67017916725cc1
lupodellasleppa <lupodellasleppa@gmail.com>
2019-10-20 23:21:01 +0200

    Edited service and added main.sh
    So service can be started with arguments

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+while [ $1 ];do
+  case $1 in
+    'auto' )
+      MODE=$1
+      PROGRAM_NUMBER=$2
+      shift
+      ;;
+    'manual' )
+      MODE=$1
+      shift
+      ;;
+  esac
+done
+
+/usr/bin/python3 /home/pi/raspb-scripts/home_control.py $MODE $PROGRAM_NUMBER
