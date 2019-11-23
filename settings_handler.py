b94d6a98c2da3449ca7b39edbd6656cafb2148e8
lupodellasleppa <lupodellasleppa@gmail.com>
2019-11-23 20:40:30 +0100

    Added some parsable arguments

--- settings_handler.py
+++ settings_handler.py
@@ -4,17 +4,22 @@ import argparse
 import json
 import logging
 import os
+import util
+
 
 logger_name = 'thermostat'
-logging.basicConfig(
-    format='{levelname:<8} {asctime} - {message}',
-    style='{'
-)
+# logging.basicConfig(
+#     format='{levelname:<8} {asctime} - {message}',
+#     style='{'
+# )
 logger = logging.getLogger(logger_name)
-logger.setLevel(logging.INFO)
+# logger.setLevel(logging.INFO)
 
 
 def create_parser():
+    '''
+    Defines script's arguments.
+    '''
 
     parser = argparse.ArgumentParser(
         description=(
@@ -23,18 +28,30 @@ def create_parser():
     )
 
     parser.add_argument(
-        "-m", "--manual",
-        help="Set manual to ON. Turns ON heater."
+        '-a', '--auto',
+        help='Set auto to ON. Turns ON heater according to program.'
     )
 
     parser.add_argument(
-        "-a", "--auto",
-        help="Set auto to ON. Turns ON heater according to program."
+        '-l', '--loglevel',
+        help='Defines the log level for logger in scripts.',
+        type=str
     )
 
     parser.add_argument(
-        "-p", "--program",
-        help="Insert the program number you wish to load.",
+        '-m', '--manual',
+        help='Set manual to ON. Turns ON heater.'
+    )
+
+    parser.add_argument(
+        '-p', '--program',
+        help='Insert the program number you wish to load.',
+        type=int
+    )
+
+    parser.add_argument(
+        '-t', '--temperature',
+        help='Insert the desired temperature when in manual mode.',
         type=int
     )
 
@@ -57,6 +74,16 @@ def create_parser():
         else:
             pass
 
+    def int_to_string(arg):
+
+        if arg is not None:
+            if isinstance(arg, int):
+                return str(arg)
+            else:
+                raise parser.error(
+                    'Please enter an integer.'
+                )
+
     args.manual = string_to_bool(args.manual)
     args.auto = string_to_bool(args.auto)
 
@@ -64,9 +91,15 @@ def create_parser():
         f'manual: {args.manual}; auto: {args.auto}; program: {args.program};'
     )
 
-    if args.manual is None and args.auto is None and args.program is None:
+    if (
+        args.auto is None and
+        args.loglevel is None and
+        args.manual is None and
+        args.program is None and
+        args.temperature is None
+    ):
         raise parser.error(
-            "Please enter at least one argument."
+            'Please enter at least one argument.'
             " Type '--help' for more information."
         )
 
@@ -74,37 +107,55 @@ def create_parser():
 
 
 def main(time_elapsed=None):
+    '''
+    Function called when script is called directly.
+    Takes argument from argparse defined in create_parser function.
+    '''
 
     args = create_parser()
 
     settings_path = 'settings.json'
     settings_file = load_settings(settings_path)
     settings_changes = {
-        "program": (
-            args.program if args.program is not None
-            else settings_file['program']
-        ),
-        "auto": (
+        'auto': (
             args.auto if args.auto is not None
             else settings_file['auto']
         ),
-        "manual": (
+        'loglevel': (
+            args.loglevel if args.loglevel is not None
+            else settings_file['loglevel']
+        )
+        'manual': (
             args.manual if args.manual is not None
             else settings_file['manual']
+        ),
+        'program': (
+            args.program if args.program is not None
+            else settings_file['program']
+        ),
+        'temperature': (
+            args.temperature if args.temperature is not None
+            else settings_file['temperature']
         )
     }
 
-    handler(settings_changes=settings_changes)
+    return handler(settings_changes=settings_changes)
 
 
 def load_settings(settings_path):
+    '''
+    Return setting in 'setting.json' file in a python dictionary.
+    '''
 
     if not os.path.isfile(settings_path) or not os.stat(settings_path).st_size:
         settings_file = {
-            "program": 0,
-            "auto": False,
-            "manual": False,
-            "time_to_wait": 1
+            'program': '0',
+            'auto': False,
+            'last_day_on': util.get_now()['format_date'],
+            'loglevel': 'INFO',
+            'manual': False,
+            'time_elapsed': '0:00:00',
+            'time_to_wait': 1
         }
         with open(settings_path, 'w') as f:
             f.write(json.dumps(settings_file, indent=2))
@@ -117,21 +168,29 @@ def load_settings(settings_path):
 
 
 def update_settings(settings_changes, settings_file, settings_path):
+    '''
+    Write setting_changes to 'setting.json' file.
+    Returns contents of the file in a python dictionary.
+    '''
 
     if settings_changes != settings_file:
         settings_changes = json.dumps(settings_changes, indent=2)
         logger.debug('\n' + settings_changes)
-        logger.debug("Settings changed!")
+        logger.debug('Settings changed!')
         with open(settings_path, 'w') as f:
             f.write(settings_changes)
             f.write('\n')
     else:
-        logger.debug("Settings not changed.")
+        logger.debug('Settings not changed.')
 
     return load_settings(settings_path)
 
 
 def handler(settings_changes={}):
+    '''
+    Sends updates to update_settings function.
+    Returns contents of 'settings.json' after updates in a python dictionary.
+    '''
 
     settings_path = 'settings.json'
     settings_file = load_settings(settings_path)
