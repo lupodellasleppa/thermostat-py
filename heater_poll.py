c06394a61e2af5c6e611d840eba16f179899e20c
lupodellasleppa <lupodellasleppa@gmail.com>
2019-11-24 23:27:12 +0100

    Implements reading temperature from device via UDP

--- heater_poll.py
+++ heater_poll.py
@@ -4,6 +4,7 @@ import argparse
 import datetime
 import json
 import logging
+import socket
 import time
 
 from heater_program import Program
@@ -12,7 +13,17 @@ import settings_handler
 import util
 
 
-def poll(settings_path, time_elapsed, heater_switch, current):
+def poll(
+    settings_path,
+    time_elapsed,
+    heater_switch,
+    current,
+    loop_count,
+    thermometer_poll,
+    UDP_port,
+    UDP_IP,
+    thermometer
+):
 
     settings = settings_handler.handler(
         settings_path=args.settings_path,
@@ -28,38 +39,57 @@ def poll(settings_path, time_elapsed, heater_switch, current):
     time_to_wait = settings['time_to_wait']
     logger.debug('time to wait: {}'.format(time_to_wait))
 
-    if manual:
-        if not heater_switch.stats: # heater is not ON
-            heater_switch.on()
-        heater_switch.catch_sleep(time_to_wait)
-        return time_to_wait
-
-    elif auto:
-        program = Program(settings['program'])
-        logger.debug(f'Loaded program {program.program_number}.')
-        logger.debug(
-            f"It is {current['formatted_time']} on {current['weekday'].title()}."
+    if not loop_count or not loop_count % thermometer_poll:
+        thermometer.sendto(b'temps_req', (UDP_IP, UDP_port))
+        temperature = json.loads(thermometer.recv(4096).decode())['celsius']
+        logger.info(
+            'Temperature from thermometer is: {}° celsius.'.format(temperature)
         )
-        program_now = program.program[current['weekday']][str(
-            current['hours']
-        )]
-        logger.debug('Program is now: {}'.format(program_now))
-        # program_vs_relay
-        if program_now:
-            if not heater_switch.stats:
-                # start it
-                logger.debug(f'Heater switch stats: {heater_switch.stats}')
-                logger.debug('Received signal to turn heater ON.')
+
+    logger.info(
+        'Room temperature: {}\n'
+        'Type of data: {}\n'
+        'Desired temperature: {}\n'
+        'Type of data: {}\n'.format(
+            temperature,
+            type(temperature),
+            settings['temperature'],
+            type(settings['temperature'])
+        )
+    )
+    if temperature < settings['temperature']:
+        if manual:
+            if not heater_switch.stats: # heater is not ON
                 heater_switch.on()
             heater_switch.catch_sleep(time_to_wait)
             return time_to_wait
-        else:
-            if heater_switch.stats:
-                # stop it
-                logger.debug('Received signal to turn heater OFF.')
-                heater_switch.off()
-            heater_switch.catch_sleep(time_to_wait)
-            return 0
+
+        elif auto:
+            program = Program(settings['program'])
+            logger.debug(f'Loaded program {program.program_number}.')
+            logger.debug(
+                f"It is {current['formatted_time']} on {current['weekday'].title()}."
+            )
+            program_now = program.program[current['weekday']][str(
+                current['hours']
+            )]
+            logger.debug('Program is now: {}'.format(program_now))
+            # program_vs_relay
+            if program_now:
+                if not heater_switch.stats:
+                    # start it
+                    logger.debug(f'Heater switch stats: {heater_switch.stats}')
+                    logger.debug('Received signal to turn heater ON.')
+                    heater_switch.on()
+                heater_switch.catch_sleep(time_to_wait)
+                return time_to_wait
+            else:
+                if heater_switch.stats:
+                    # stop it
+                    logger.debug('Received signal to turn heater OFF.')
+                    heater_switch.off()
+                heater_switch.catch_sleep(time_to_wait)
+                return 0
 
     else:
         if heater_switch.stats:
@@ -72,6 +102,14 @@ def poll(settings_path, time_elapsed, heater_switch, current):
 def main(settings_path):
 
     heater_switch = Relay('36')
+    # parameters for UDP connection with thermometer
+    UDP_port = 4210
+    UDP_IP = '192.168.1.112'
+    thermometer = socket.socket(socket.SF_INET, socket.SOCK_DGRAM)
+    thermometer.settimeout(1.0)
+    thermometer.bind(('', UDP_port))
+    thermometer_poll = 5
+    # load settings
     settings = settings_handler.load_settings(settings_path)
     if settings['last_day_on'] != util.get_now()['formatted_date']:
         time_elapsed = 0
@@ -90,7 +128,10 @@ def main(settings_path):
         minutes=time_elapsed_restore.minute,
         seconds=time_elapsed_restore.second
     ).total_seconds())
+
     last_current = None
+
+    loop_count = 0
 
     while True:
 
@@ -111,9 +152,15 @@ def main(settings_path):
             settings_path,
             util.format_seconds(time_elapsed),
             heater_switch,
-            current
+            current,
+            loop_count,
+            thermometer_poll,
+            UDP_port,
+            UDP_IP,
+            thermometer
         )
         last_current = current
+        loop_count += 1
 
 
 if __name__ == '__main__':
